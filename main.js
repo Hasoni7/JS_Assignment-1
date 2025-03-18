1982269b1260757b331560ab6a7dc0c532e95f43
Hasoni7 <Hasan.oda@outlook.com>
2025-03-18 17:42:21 +0100

    added some java code
    Print "Hello, World!"
    Created variables and used them.
    Added basic arithmetic operations

---
+++ main.js
@@ -0,0 +1,22 @@
+// first exercise
+
+console.log("Hello, world!");
+
+// second exercise
+let x = "Hasan Hashem";
+let y = "30 years old";
+let favColor = "green";
+
+console.log(
+  "My name is " + x + ", I am " + y + "and my favorite color is " + favColor
+);
+
+// third exercise
+
+let a = 10;
+let b = 20;
+
+console.log(a + b); // addition
+console.log(a - b); // subtraction
+console.log(a * b); // multiplication
+console.log(a / b); // division
